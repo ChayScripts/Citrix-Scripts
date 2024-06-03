07ae40cd1d134b187f4df2cc2ced8e2e922b8a55
Chay <43686905+ChayScripts@users.noreply.github.com>
2024-06-03 23:17:36 +0530

    Update Restart VDI.ps1

--- Restart VDI.ps1
+++ Restart VDI.ps1
@@ -1,6 +1,6 @@
-#Create schedule task with the script. It sends messages to logged on users every 5 minutes and logs off user's session after 30 mins.
+#Create schedule task with the script. It sends messages to all logged on users every 1 minute and logs off user's session after 5 mins.
 
-$Timer = 2
+$Timer = 5
 while (Invoke-Command -ScriptBlock { quser } ) {
     if ($Timer -ne "0") {
         msg * "This machine will be restarted in $Timer mins. Please save your work and logoff."
